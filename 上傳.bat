b37cf9cd2f62fae44e24b50499a68305eabac4aa
chenkohan <ko1962000@gmail.com>
2023-03-18 16:28:30 +0800

    Initial commit

--- 上傳.bat
+++ 上傳.bat
@@ -1,12 +1,13 @@
 @echo off
 
-set /p email=ko1962000@gmail.com: 
-set /p name=chenkohan: 
+set email=ko1962000@gmail.com
+set name=chenkohan
+set repo_url=https://github.com/your-username/your-repo.git
 
 git init
 git add .
 git commit -m "Initial commit"
-git remote add origin https://github.com/chenkohan/VCF
+git remote add origin %repo_url%
 git config --global user.email "%email%"
 git config --global user.name "%name%"
 git push -u origin main
@@ -15,6 +16,7 @@ echo Git identity set:
 git config --list | findstr user.
 
 pause
+
 
 
 
